8f077f28bef9fcbf008df1c1360135c464ae99a8
Pablo Ventura <pablogventura@gmail.com>
2022-11-06 09:51:00 -0300

    svg

--- automata.py
+++ automata.py
@@ -18,6 +18,12 @@ class Delta(object):
         for q in self.estados:
             for a in self.alfabeto:
                 pass
+    
+    def _repr_svg_(self):
+        return r"""<svg height="100" width="100">
+  <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+  Sorry, your browser does not support inline SVG.  
+</svg>"""
 
         
 
