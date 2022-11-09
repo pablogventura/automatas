8e28125fc4f6256619d82af5ffb5e2e6e92c08e4
Pablo Ventura <pablogventura@gmail.com>
2022-11-08 23:01:35 -0300

    tikz

--- automata.py
+++ automata.py
@@ -1,7 +1,37 @@
 import numpy
 import re
 from  IPython.display import Math
-from . import tikz
+import subprocess
+
+class Tikz(object):
+
+    def __init__(self, code, tikzlibraries, tikzpictureoptions):
+        self.code = code
+        self.tikzlibraries = tikzlibraries
+        self.tikzpictureoptions = tikzpictureoptions
+
+    def latex(self):
+        return (r"""
+\documentclass[convert={convertexe={convert},size=400x240,outext=.png},border=0pt]{standalone}
+\usepackage[]{tikz}
+\usetikzlibrary{""" + self.tikzlibraries + r"""}
+
+\begin{document}
+\begin{tikzpicture}[""" + self.tikzpictureoptions + r"""]
+""" + self.code + r"""
+\end{tikzpicture}
+\end{document}
+""").encode()
+
+    def svg(self):
+        subprocess.run(["latex"],capture_output=True,input=self.latex())
+        subprocess.run(["dvisvgm", "--zoom=-1", "--exact", "--font-format=woff", "texput.dvi"],capture_output=True)
+        with open("/content/texput.svg","r") as f:
+            svg_code = f.read()
+        subprocess.call(["rm", "/content/texput.svg", "/content/texput.aux", "/content/texput.dvi", "/content/texput.log"])
+        return svg_code
+
+
 
 class Delta(object):
     def __init__(self,tabla):
@@ -76,4 +106,4 @@ class DFA(object):
 (f) edge [right]   node         {$\varepsilon$} (i);
 """
 
-        return tikz.Tikz(c,"positioning,automata","on top/.style={preaction={draw=white,-}},on top/.default=4pt").svg()
+        return Tikz(c,"positioning,automata","on top/.style={preaction={draw=white,-}},on top/.default=4pt").svg()
