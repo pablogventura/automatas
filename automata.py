05dbd38e6d6d7d076026d17e41b4ebb7020f6066
Pablo Ventura <pablogventura@gmail.com>
2022-11-14 08:33:28 -0300

    paso a usar graphviz

--- automata.py
+++ automata.py
@@ -1,46 +1,6 @@
 import numpy
 import re
-from  IPython.display import Math
-import subprocess
-from math import sqrt
-
-class Tikz(object):
-
-    def __init__(self, code, tikzlibraries, tikzpictureoptions):
-        self.code = code
-        self.tikzlibraries = tikzlibraries
-        self.tikzpictureoptions = tikzpictureoptions
-
-    def latex(self):
-        return (r"""
-\documentclass[convert={convertexe={convert},size=400x240,outext=.png},border=0pt]{standalone}
-\usepackage[]{tikz}
-\usetikzlibrary{automata,positioning,arrows}
-\tikzset{
-->, % makes the edges directed
-%>=stealth’, % makes the arrow heads bold
-node distance=3cm, % specifies the minimum distance between two nodes. Change if necessary.
-every state/.style={thick, fill=gray!10}, % sets the properties for each ’state’ node
-initial text=$ $, % sets the text that appears on the start arrow
-}
-\usetikzlibrary{""" + self.tikzlibraries + r"""}
-
-\begin{document}
-\begin{tikzpicture}[""" + self.tikzpictureoptions + r"""]
-""" + self.code + r"""
-\end{tikzpicture}
-\end{document}
-""").encode()
-
-    def svg(self):
-        subprocess.run(["latex"],capture_output=True,input=self.latex())
-        subprocess.run(["dvisvgm", "--zoom=1", "--exact", "--font-format=woff", "texput.dvi"],capture_output=True)
-        with open("/content/texput.svg","r") as f:
-            svg_code = f.read()
-        subprocess.call(["rm", "/content/texput.svg", "/content/texput.aux", "/content/texput.dvi", "/content/texput.log"])
-        return svg_code
-
-
+import graphviz
 
 class Delta(object):
     def __init__(self,tabla):
@@ -122,93 +82,26 @@ class DFA(object):
             return ", right of=" + places[(x-1,y)]
         else:
             return ", below of=" + places[(x,y-1)]
-    def _arrow(self,q0,q1,places):
-        x_q0,y_q0 = list(places.keys())[list(places.values()).index(q0)]
-        x_q1,y_q1 = list(places.keys())[list(places.values()).index(q1)]
-        distancia = sqrt((x_q1-x_q0)**2+(y_q1-y_q0)**2)
-        if distancia > 1:
-            modificador = ", bend left, left=" +  str(round(distancia**2,2))
-        else:
-            modificador = ""
-
-        syms = self.delta._get_steps(q0,q1)
-        if syms:
-            return {r"  (" + q0 + r") edge[above" + modificador + r"] node{$" + ", ".join(syms) + r"$} (" + q1 + r")" + "\n"}
-        else:
-            return {}
-
-    def _arrow_loop(self,q,places):
-        syms = self.delta._get_steps(q,q)
-        if syms:
-            return {r"  (" + q + r") edge[loop above, above] node{$" + ", ".join(syms) + r"$} (" + q + r")" + "\n"}
-        else:
-            return {}
-    def _arrows(self,q0,q1,places):
-        result = set()
-
-        result = result.union(self._arrow(q0,q1,places))
-        result = result.union(self._arrow(q1,q0,places))
-        result = result.union(self._arrow_loop(q0,places))
-        result = result.union(self._arrow_loop(q1,places))
-        return result
-
     def _generate_tikz(self):
-        places = dict()
-        node_x=0
-        node_y=0
-        c=""
+        graph = graphviz.Digraph(comment='DFA')
+        graph.graph_attr['rankdir'] = 'LR'
+        graph.node("fake",style="invisible",root="true")
         for q in self.estados:
-            modifiers = self.find_place(q,node_x,node_y,places)
             if q in self.finales:
-                modifiers+= ", accepting"
-            if q == self.finales:
-                modifiers+= ", initial"
-            
-            c+=r"\node[state" + modifiers + r"] ("+ q + r") {$"+ self.delta._prettify_name(q) + r"$};" + "\n"
-            
-            node_x = (node_x + 1) % 4
-            if node_x == 0:
-                node_y += 1
-        c += r"\draw "
-        arrows = set()
-        for q0 in self.estados:
-            for q1 in self.estados:
-                if not q0 < q1:
-                    continue
-                arrows = arrows.union(self._arrows(q0,q1,places))
-        c += "".join(arrows)
-        c += ";"
-        self.tikz = Tikz(c,"","")
+                graph.node(q,label=self.delta._prettify_name(q),shape="doublecircle")
+            else:
+                graph.node(q,label=self.delta._prettify_name(q),shape="circle")
+        graph.edge("fake",self.inicial,style="bold")
+        for q in self.estados:
+            for a in self.alfabeto:
+                graph.edge(q, self.delta(q,a), label=a)
+        self.graph = graph
 
     def _repr_latex_(self):
-        return self.tikz.latex()
+        return self.graph.source
     def _repr_svg_(self):
-        return self.tikz.svg()
+        return self.graph._repr_svg_()
 
-
-#         c=r"""\node[state] (phi) {$\emptyset$};
-# \node[state, accepting, right of=phi] (1) {$\{1\}$};
-# \node[state, right of=1] (2) {$\{2\}$};
-# \node[state, accepting, right of=2] (12) {$\{1, 2\}$};
-# \node[state, below of=phi] (3) {$\{3\}$};
-# \node[state, initial, initial where=above, accepting, right of=3] (13) {$\{1, 3\}$};
-# \node[state, right of=13] (23) {$\{2, 3\}$};
-# \node[state, accepting, right of=23] (123) {$\{1, 2, 3\}$};
-# \draw (phi) edge[loop left] node{$a, b$} (phi)
-#     (1) edge[above] node{$a$} (phi)
-#     (1) edge[above] node{$b$} (2)
-#     (2) edge[right] node{$a$} (23)
-#     (2) edge[above] node{$b$} (3)
-#     (12) edge[above, pos=.3, left=2pt] node{$a, b$} (23)
-#     (3) edge[left] node{$b$} (phi)
-#     (3) edge[below] node{$a$} (13)
-#     (13) edge[loop right] node{$a$} (13)
-#     (13) edge[above] node{$b$} (2)
-#     (23) edge[bend left, above] node{$a$} (123)
-#     (23) edge[bend left, below] node{$b$} (3)
-#     (123) edge[loop above] node{$a$} (123)
-#     (123) edge[bend left, below] node{$b$} (23);
-#"""
 
 if __name__ == "__main__":
     f="""
@@ -236,4 +129,4 @@ if __name__ == "__main__":
 
     a = DFA(d,"q0",{"q2"})
 
-    print(a._repr_latex_().decode())
+    print(a._repr_latex_())
